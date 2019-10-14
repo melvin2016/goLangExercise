ce96a30e35b4c5e50f251b8235ad2c8e8065fb50
MELVIN GEORGE <melvingeorge10@gmail.com>
2019-10-15 00:15:32 +0530

    adding interface type , just basics - added comments to make code readable

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,11 +1,21 @@
+// Interface types are a contract to make code better and manageable.
 package main
 
 import "fmt"
 
+// house interface giving out
+// specific information of
+// getFloorNumber Function.
 type house interface {
 	getFloorNumber() int
 }
+
+// struct flat which implements the house interface,
+// because it defines getFloorNumber() function.
 type flat struct{}
+
+// struct bunker which implements the house interface,
+// because it defines getFloorNumber() function.
 type bunker struct{}
 
 func main() {
@@ -15,14 +25,21 @@ func main() {
 	printFloorNumber(bk)
 }
 
+// function of flat struct implementing the
+// getFloorNumber() function of house interface.
 func (flat) getFloorNumber() int {
 	return 32
 }
 
+// function of bunker struct implementing the
+// getFloorNumber() function of house interface.
 func (bunker) getFloorNumber() int {
 	return 0
 }
 
+// Interface house helps in not writing
+// 2 different function for 2 different
+// structs since both use house interface as base.
 func printFloorNumber(h house) {
 	fmt.Println("The floor number is: ", h.getFloorNumber())
 }
