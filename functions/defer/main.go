2eb5f1522c94432d076d579b10a93dfd9a3d018d
MELVIN GEORGE <melvingeorge10@gmail.com>
2019-10-27 17:32:35 +0530

    defer keyword used in function call

---
+++ functions/defer/main.go
@@ -0,0 +1,27 @@
+// Explanation On defer keyword
+package main
+
+import "fmt"
+
+func main() {
+	// defer keyword is used to postpone a function
+	// call to the exit of its lexical function scope.
+	// so that world() function will be called at exit of main function.
+	// and hello() function will be called first.
+	/* -------------------------- */
+	// Useful for readability of code
+	// most commonly used with file closing.
+	// eg :
+	// file.open()
+	// defer file.close() -- useful for code readability.
+	defer world()
+	hello()
+}
+
+func hello() {
+	fmt.Println("Hello")
+}
+
+func world() {
+	fmt.Println("world!")
+}
